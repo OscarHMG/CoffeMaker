ba476722b2b4d2925077f5d8a36e776f269d0a10
OscarHMG <oscar_mg7@hotmail.com>
2016-02-16 18:05:19 -0500

    Added POST request to thingspeak

--- app/src/main/java/com/example/user/coffeemaker/MainActivity.java
+++ app/src/main/java/com/example/user/coffeemaker/MainActivity.java
@@ -21,10 +21,17 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
+import java.net.ProtocolException;
 import java.net.URL;
+import java.net.URLEncoder;
 import java.util.Calendar;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 import cn.pedant.SweetAlert.SweetAlertDialog;
 import xyz.hanks.library.SmallBang;
 import xyz.hanks.library.SmallBangListener;
@@ -32,8 +39,8 @@ import xyz.hanks.library.SmallBangListener;
 /**
  * Created by user on 10/02/2016.
  */
-public class MainActivity extends Activity implements View.OnClickListener, SmallBangListener{
-    private RelativeLayout alarm,turn,temperature,status,about;
+public class MainActivity extends Activity implements View.OnClickListener, SmallBangListener {
+    private RelativeLayout alarm, turn, temperature, status, about;
     private TableLayout menu;
     private SmallBang animation;
     private int hourOfDay, minuteOfDay;
@@ -42,13 +49,14 @@ public class MainActivity extends Activity implements View.OnClickListener, Smal
     private Intent myIntent;
     private PendingIntent pendingIntent;
     private ConnectionDetector connection;
+    private static String typeOfOp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(com.example.user.coffeemaker.R.layout.menu);
         initElementes();
-        connection=new ConnectionDetector(MainActivity.this);
+        connection = new ConnectionDetector(MainActivity.this);
         animation = SmallBang.attach2Window(this);
         setAnimations();
     }
@@ -75,7 +83,7 @@ public class MainActivity extends Activity implements View.OnClickListener, Smal
     @Override
     public void onClick(View v) {
 
-        if(!connection.isConnectingToInternet()){
+        if (!connection.isConnectingToInternet()) {
             new SweetAlertDialog(this, SweetAlertDialog.WARNING_TYPE)
                     .setTitleText("UPS!")
                     .setContentText("Check your internet Connection")
@@ -87,26 +95,28 @@ public class MainActivity extends Activity implements View.OnClickListener, Smal
                         }
                     })
                     .show();
-        }else {
+        } else {
             switch (v.getId()) {
                 case com.example.user.coffeemaker.R.id.containter_alarm:
                     animation.bang(alarm, 260, this);
-
+                    typeOfOp = "POST";
                     showDialog(999);
                     break;
                 case com.example.user.coffeemaker.R.id.containter_turn:
                     animation.bang(turn, 260, this);
                     showDialogMessage("Actual State: ON/OFF?", "Succesfully ON/OFF ?", SweetAlertDialog.SUCCESS_TYPE);
+                    typeOfOp = "POST";
+                    new JSonTask().execute("http://api.thingspeak.com/channels/81636/fields/1/last.txt");
                     break;
                 case com.example.user.coffeemaker.R.id.container_temperature:
                     animation.bang(temperature, 260, this);
-
+                    typeOfOp = "GET";
                     new JSonTask().execute("http://api.thingspeak.com/channels/81636/fields/1/last.txt");
-
                     break;
                 case com.example.user.coffeemaker.R.id.containter_status:
                     animation.bang(status, 260, this);
                     showDialogMessage("Status", "The jug is not placed in the coffee maker?", SweetAlertDialog.WARNING_TYPE);
+                    typeOfOp = "GET";
                     break;
                 case com.example.user.coffeemaker.R.id.containter_about:
                     animation.bang(about, 260, this);
@@ -116,28 +126,31 @@ public class MainActivity extends Activity implements View.OnClickListener, Smal
         }
     }
 
-    public void setDisableStatusBar(){
+    public void setDisableStatusBar() {
         View decorView = getWindow().getDecorView();
         int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
         decorView.setSystemUiVisibility(uiOptions);
     }
-    public void setAnimations(){
+
+    public void setAnimations() {
         setAnimationView(alarm);
         setAnimationView(turn);
         setAnimationView(temperature);
         setAnimationView(status);
         setAnimationView(about);
     }
-    public void setAnimationView(View v){
+
+    public void setAnimationView(View v) {
         PropertyAction alarmAnimation = PropertyAction.
-                                        newPropertyAction(v).scaleX(0).scaleY(0).duration(1000).
-                                        interpolator(new AccelerateDecelerateInterpolator()).build();
+                newPropertyAction(v).scaleX(0).scaleY(0).duration(1000).
+                interpolator(new AccelerateDecelerateInterpolator()).build();
         Player.init().animate(alarmAnimation).play();
     }
 
-    public void setAlarm(){
+    public void setAlarm() {
         final int hour = hourOfDay;
-        final int minute = minuteOfDay;;
+        final int minute = minuteOfDay;
+        ;
 
         String minute_string = String.valueOf(minute);
         String hour_string = String.valueOf(hour);
@@ -147,19 +160,20 @@ public class MainActivity extends Activity implements View.OnClickListener, Smal
         }
 
         pendingIntent = PendingIntent.getBroadcast(this, 0, myIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-        alarmManager.set(AlarmManager.RTC_WAKEUP,calendar.getTimeInMillis(),pendingIntent);
-        Toast.makeText(this,"Alarm set to "+hour_string+":"+minute_string,Toast.LENGTH_LONG).show();
+        alarmManager.set(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pendingIntent);
+        Toast.makeText(this, "Alarm set to " + hour_string + ":" + minute_string, Toast.LENGTH_LONG).show();
     }
 
     @Override
-    public Dialog onCreateDialog(int id){
-        if(id==999){
+    public Dialog onCreateDialog(int id) {
+        if (id == 999) {
             hourOfDay = calendar.getTime().getHours();
             minuteOfDay = calendar.getTime().getMinutes();
-            return new TimePickerDialog(this, AlertDialog.THEME_DEVICE_DEFAULT_LIGHT,timePickerListener, hourOfDay,minuteOfDay, false);
+            return new TimePickerDialog(this, AlertDialog.THEME_DEVICE_DEFAULT_LIGHT, timePickerListener, hourOfDay, minuteOfDay, false);
         }
         return null;
     }
+
     private TimePickerDialog.OnTimeSetListener timePickerListener = new TimePickerDialog.OnTimeSetListener() {
         @Override
         public void onTimeSet(TimePicker timePicker, int hour, int minute) {
@@ -170,8 +184,8 @@ public class MainActivity extends Activity implements View.OnClickListener, Smal
         }
     };
 
-    public void showDialogMessage(String title, String context, int typeMessage){
-        new SweetAlertDialog(this,typeMessage)
+    public void showDialogMessage(String title, String context, int typeMessage) {
+        new SweetAlertDialog(this, typeMessage)
                 .setTitleText(title)
                 .setContentText(context)
                 .show();
@@ -187,39 +201,125 @@ public class MainActivity extends Activity implements View.OnClickListener, Smal
 
     }
 
+    public void postValue() {
 
+    }
 
-    public class JSonTask extends AsyncTask<String, String, String>{
+    public class JSonTask extends AsyncTask<String, String, String> {
 
 
         @Override
         protected String doInBackground(String... params) {
+            if (typeOfOp.equals("GET")) {
+                return getRequest(params);
+            } else {
+                postRequest();
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            showDialogMessage("Current Temperature", "Temperature:" + s, SweetAlertDialog.SUCCESS_TYPE);
+        }
+
+        private String postRequest() {
+            URL url = null;
+            try {
+                url = new URL("https://api.thingspeak.com/update");
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+            Map<String, Object> params = new LinkedHashMap<>();
+            params.put("api_key", "JOXZJ0MWXKS66ENR");
+            params.put("field1", "100");
+
+            StringBuilder postData = new StringBuilder();
+            for (Map.Entry<String, Object> param : params.entrySet()) {
+                if (postData.length() != 0) postData.append('&');
+                try {
+                    postData.append(URLEncoder.encode(param.getKey(), "UTF-8"));
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                }
+                postData.append('=');
+                try {
+                    postData.append(URLEncoder.encode(String.valueOf(param.getValue()), "UTF-8"));
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                }
+            }
+            byte[] postDataBytes = new byte[0];
+            try {
+                postDataBytes = postData.toString().getBytes("UTF-8");
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+
+            HttpURLConnection conn = null;
+            try {
+                conn = (HttpURLConnection) url.openConnection();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                conn.setRequestMethod("POST");
+            } catch (ProtocolException e) {
+                e.printStackTrace();
+            }
+            conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+            conn.setRequestProperty("Content-Length", String.valueOf(postDataBytes.length));
+            conn.setDoOutput(true);
+            try {
+                conn.getOutputStream().write(postDataBytes);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            Reader in = null;
+            try {
+                in = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                for (int c = in.read(); c != -1; c = in.read())
+                    System.out.print((char) c);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return "";
+        }
+
+
+        public String getRequest(String... params) {
             HttpURLConnection connection = null;
             StringBuffer buffer;
             BufferedReader reader = null;
             URL url;
             InputStream stream;
-            try{
+            try {
                 url = new URL(params[0]);
-                connection = (HttpURLConnection)url.openConnection();
+                connection = (HttpURLConnection) url.openConnection();
                 connection.connect();
                 stream = connection.getInputStream();
                 reader = new BufferedReader(new InputStreamReader(stream));
                 buffer = new StringBuffer();
                 String line = "";
-                while((line = reader.readLine())!=null){
+                while ((line = reader.readLine()) != null) {
                     buffer.append(line);
                 }
                 return buffer.toString();
-            }catch(MalformedURLException ex){
+            } catch (MalformedURLException ex) {
                 ex.printStackTrace();
-            }catch(IOException ioex){
+            } catch (IOException ioex) {
                 ioex.printStackTrace();
-            }finally{
-                if(connection!=null)
+            } finally {
+                if (connection != null)
                     connection.disconnect();
                 try {
-                    if(reader!=null)
+                    if (reader != null)
                         reader.close();
                 } catch (IOException e) {
                     e.printStackTrace();
@@ -228,11 +328,6 @@ public class MainActivity extends Activity implements View.OnClickListener, Smal
             return null;
         }
 
-        @Override
-        protected void onPostExecute(String s) {
-            super.onPostExecute(s);
-            showDialogMessage("Current Temperature", "Temperature:"+s, SweetAlertDialog.SUCCESS_TYPE);
-        }
     }
 
 }
