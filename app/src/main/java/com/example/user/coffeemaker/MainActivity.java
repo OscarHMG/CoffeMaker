e9fab2e94edca9c42545de404f7f1313e8bf30c2
OscarHMG <oscar_mg7@hotmail.com>
2016-02-19 13:35:35 -0500

    All is Working

--- app/src/main/java/com/example/user/coffeemaker/MainActivity.java
+++ app/src/main/java/com/example/user/coffeemaker/MainActivity.java
@@ -10,7 +10,6 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.R;
 import android.util.Log;
 import android.view.View;
 import android.view.animation.AccelerateDecelerateInterpolator;
@@ -43,8 +42,8 @@ import xyz.hanks.library.SmallBangListener;
  * Created by user on 10/02/2016.
  */
 public  class MainActivity extends Activity implements View.OnClickListener, SmallBangListener {
+    private static final int FLAG_ALARM_DIALOG =999 ;
     private RelativeLayout alarm, turn, temperature, status, about;
-    private TableLayout menu;
     private SmallBang animation;
     private int hourOfDay, minuteOfDay;
     private AlarmManager alarmManager;
@@ -54,11 +53,15 @@ public  class MainActivity extends Activity implements View.OnClickListener, Sma
     private ConnectionDetector connection;
     private static String typeOfOp;
     private static String id= "0";
-    private Context context;
     protected SweetAlertDialog dialog;
     JSonTask mWorker  = new JSonTask(this);
+    private final static String GET_URL = "http://api.thingspeak.com/channels/81636/fields";
+    private final static String POST_URL="https://api.thingspeak.com/update";
+    private final static String API_KEY= "JOXZJ0MWXKS66ENR";
 
-    protected MainActivity m;
+    protected MainActivity principalActivity;
+    private TableLayout menu;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -69,13 +72,11 @@ public  class MainActivity extends Activity implements View.OnClickListener, Sma
         setAnimations();
         mWorker.mainActivity = this;
         typeOfOp = "GET";
-        mWorker.execute("http://api.thingspeak.com/channels/81636/fields/1/last.txt");
-
-
+        mWorker.execute(GET_URL+"/1/last.txt");
     }
 
     public MainActivity() {
-        this.m = this;
+        this.principalActivity = this;
     }
 
     public void initElementes() {
@@ -88,7 +89,6 @@ public  class MainActivity extends Activity implements View.OnClickListener, Sma
         status = (RelativeLayout) findViewById(com.example.user.coffeemaker.R.id.containter_status);
         about = (RelativeLayout) findViewById(com.example.user.coffeemaker.R.id.containter_about);
         myIntent = new Intent(MainActivity.this, AlarmReceiver.class);
-
         alarm.setOnClickListener(this);
         turn.setOnClickListener(this);
         temperature.setOnClickListener(this);
@@ -119,28 +119,27 @@ public  class MainActivity extends Activity implements View.OnClickListener, Sma
                     animation.bang(alarm, 260, this);
                     typeOfOp = "POST";
                     id="1";
-                    showDialog(999);
+                    showDialog(FLAG_ALARM_DIALOG);
                     break;
                 case com.example.user.coffeemaker.R.id.containter_turn:
                     animation.bang(turn, 260, this);
-                    String msg = (mWorker.estado.equals("1")) ? "OFF" : "ON";
+                    String msg = (mWorker.estado.equals("1")) ? " OFF" : " ON";
                     showDialogMessage("CoffeeMaker", "Succesfully " +msg, SweetAlertDialog.SUCCESS_TYPE);
                     typeOfOp = "POST";
                     id="2";
-                    JSonTask w1= (JSonTask) new JSonTask(this).execute("2");
+                    JSonTask turnTask= (JSonTask) new JSonTask(this).execute("2");
                     break;
                 case com.example.user.coffeemaker.R.id.container_temperature:
                     animation.bang(temperature, 260, this);
                     typeOfOp = "GET";
                     id="3";
-                    JSonTask w2= (JSonTask) new JSonTask(this).execute("http://api.thingspeak.com/channels/81636/fields/2/last.txt");
-
+                    JSonTask getTempTask= (JSonTask) new JSonTask(this).execute(GET_URL+"2/last.txt");
                     break;
                 case com.example.user.coffeemaker.R.id.containter_status:
                     animation.bang(status, 260, this);
                     typeOfOp = "GET";
                     id="4";
-                    JSonTask w3= (JSonTask) new JSonTask(this).execute("http://api.thingspeak.com/channels/81636/fields/3/last.txt");
+                    JSonTask getStatusTask= (JSonTask) new JSonTask(this).execute(GET_URL+"/3/last.txt");
                     break;
                 case com.example.user.coffeemaker.R.id.containter_about:
                     animation.bang(about, 260, this);
@@ -172,27 +171,26 @@ public  class MainActivity extends Activity implements View.OnClickListener, Sma
     }
 
     public void setAlarm() {
-        final int hour = hourOfDay;
-        final int minute = minuteOfDay;
-        ;
+        int x =calendar.HOUR_OF_DAY;
+        int y =calendar.MINUTE;
+        calendar.set(Calendar.HOUR_OF_DAY, hourOfDay);
+        calendar.set(Calendar.MINUTE, minuteOfDay);
 
-        String minute_string = String.valueOf(minute);
+       /* String minute_string = String.valueOf(minute);
         String hour_string = String.valueOf(hour);
 
         if (minute < 10) {
             minute_string = "0" + String.valueOf(minute);
-        }
+        }*/
 
         pendingIntent = PendingIntent.getBroadcast(this, 0, myIntent, PendingIntent.FLAG_UPDATE_CURRENT);
         alarmManager.set(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pendingIntent);
-        Toast.makeText(this, "Alarm set to " + hour_string + ":" + minute_string, Toast.LENGTH_LONG).show();
+        //Toast.makeText(this, "Alarm set to " + hour_string + ":" + minute_string, Toast.LENGTH_LONG).show();
     }
 
     @Override
     public Dialog onCreateDialog(int id) {
         if (id == 999) {
-            hourOfDay = calendar.getTime().getHours();
-            minuteOfDay = calendar.getTime().getMinutes();
             return new TimePickerDialog(this, AlertDialog.THEME_DEVICE_DEFAULT_LIGHT, timePickerListener, hourOfDay, minuteOfDay, false);
         }
         return null;
@@ -201,10 +199,10 @@ public  class MainActivity extends Activity implements View.OnClickListener, Sma
     private TimePickerDialog.OnTimeSetListener timePickerListener = new TimePickerDialog.OnTimeSetListener() {
         @Override
         public void onTimeSet(TimePicker timePicker, int hour, int minute) {
-            hourOfDay = hour;
+            hourOfDay =  hour;
             minuteOfDay = minute;
             setAlarm();
-            showDialogMessage("Alarm", "Alarm settings was successful", SweetAlertDialog.SUCCESS_TYPE);
+            showDialogMessage("Alarm", "Alarm settings was successful ", SweetAlertDialog.SUCCESS_TYPE);
         }
     };
 
@@ -263,6 +261,7 @@ public  class MainActivity extends Activity implements View.OnClickListener, Sma
             switch(id){
                 case "0":
                     mainActivity.showDialogMessage("Welcome", "", SweetAlertDialog.SUCCESS_TYPE);
+                    estado= s; //App start, previosuly we have to know if the cofee maker is ON or OFF
                     break;
                 case "1":
 
@@ -270,10 +269,11 @@ public  class MainActivity extends Activity implements View.OnClickListener, Sma
                 case "2":
                     break;
                 case "3":
-                    mainActivity.showDialogMessage("Actual Temperature", s, SweetAlertDialog.WARNING_TYPE);
+                    mainActivity.showDialogMessage("Actual Temperature", s+" °C", SweetAlertDialog.WARNING_TYPE);
                     break;
                 case "4":
-                    mainActivity.showDialogMessage("Status",s,SweetAlertDialog.WARNING_TYPE);
+                    String result = s.equals("1")?"The jar is present in Coffee Maker":"The jar isn't present in the Coffee Maker";
+                    mainActivity.showDialogMessage("Status",result,SweetAlertDialog.WARNING_TYPE);
                     break;
             }
             //showDialogMessage("Current Temperature", "Temperature:" + s, SweetAlertDialog.SUCCESS_TYPE);
@@ -297,11 +297,11 @@ public  class MainActivity extends Activity implements View.OnClickListener, Sma
 
         public  Map<String, Object> setParams(String id) {
             Map<String, Object> params = new LinkedHashMap<>();
-            params.put("api_key", "JOXZJ0MWXKS66ENR");
-            if (id.equals("1")) {
+            params.put("api_key", API_KEY);
+            if (id.equals("1")) { //Set Alarm
                 params.put("field1", "1");
-            } else if (id.equals("2")) {
-                JSonTask m = (JSonTask) new JSonTask(mainActivity).execute("http://api.thingspeak.com/channels/81636/fields/1/last.txt");
+            } else if (id.equals("2")) { // Turn ON or Turn OFF?
+                //JSonTask mTask = (JSonTask) new JSonTask(mainActivity).execute(GET_URL+"/1/last.txt");
                 if (estado.equals("1")) { //CoffeeMaker is ON
                     params.put("field1", "0");
                     estado = "0";
@@ -309,7 +309,7 @@ public  class MainActivity extends Activity implements View.OnClickListener, Sma
                     params.put("field1", "1");
                     estado = "1";
                 }
-                finalEstado = estado;
+                //finalEstado = estado;
                 Log.i("Estado Cambiante:",estado);
             }
             return params;
@@ -318,7 +318,7 @@ public  class MainActivity extends Activity implements View.OnClickListener, Sma
         private String postRequest(String... p) {
             URL url = null;
             try {
-                url = new URL("https://api.thingspeak.com/update");
+                url = new URL(POST_URL);
             } catch (MalformedURLException e) {
                 e.printStackTrace();
             }
@@ -400,7 +400,6 @@ public  class MainActivity extends Activity implements View.OnClickListener, Sma
                 while ((line = reader.readLine()) != null) {
                     buffer.append(line);
                 }
-                msgGET = buffer.toString();
                 return buffer.toString();
             } catch (MalformedURLException ex) {
                 ex.printStackTrace();
@@ -418,8 +417,5 @@ public  class MainActivity extends Activity implements View.OnClickListener, Sma
             }
             return null;
         }
-
-
     }
-
 }
